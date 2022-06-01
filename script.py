136ca016330ee1d94f5c01785bf8c2a98c46b95e
ankit <ankitgautam.ck@gmail.com>
2022-06-01 12:37:46 +0200

    added 3 error functions

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from tld import get_fld
 from PIL import Image
 from datetime import datetime
@@ -37,12 +38,33 @@ def main():
 
     build_url()
 
-    def check_TLS(URL):
+    def check_TLS(URL):                                         # TLS error
         try:
             response = requests.get(URL)
             return False
         except requests.exceptions.RequestException as e:
+            if 'CERTIFICATE_VERIFY_FAILED' in str(e):
+                print('TLS_error')
+            elif 'hostname' in str(e):
+                print('TLS_error')
             return True
+
+
+    def time_out(url):                              # timeout error code 
+        try:
+            driver.set_page_load_timeout(30)
+            driver.get(url)
+        except TimeoutException as e:
+            print('time_out')
+            driver.quit()
+
+    def domain_not_exit(url):      # domian does not exit we do not need this in anaylsis report
+        try:
+            response = requests.get(url)
+        except requests.exceptions.ConnectionError as e:
+                print('domain_does_not_exit')
+                driver.quit()
+
 
     #print(urls)
 
